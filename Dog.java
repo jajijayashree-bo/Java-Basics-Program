d95dcf10fd879346e7a559151300e2aa5250d1f4
Jayashree S <jajijayashree@gmail.com>
2026-02-11 10:27:08 +0530

    class and objects
    in this program am used a  basic class and object.

---
+++ Dog.java
@@ -0,0 +1,14 @@
+
+public class Dog {
+	    void sound() {
+	        System.out.println("Dog barks");
+	    }
+	}
+
+	class Main {
+	    public static void main(String[] args) {
+	        Dog d1 = new Dog();
+	        d1.sound();
+	    }
+	}
+
